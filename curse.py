d9d150dbd2e0eda201b4640eb380a50ccf58b0a9
Pete Richardson <peterichardson@apple.com>
2018-08-02 16:28:21 +0100

    a couple of windows and some text

--- curse.py
+++ curse.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 import curses
-stdscr = curses.initscr()
-curses.noecho()
-curses.cbreak()
 
-stdscr.addstr("Hello, World!")
+def main(stuff):
+    w1 = curses.newwin(5,20,0,0)
+    w2 = curses.newwin(5,20,0,21)
+    w1.addstr(0,2,"w1")
+    w2.addstr(0,2,"w2",curses.A_REVERSE)
+    w2.refresh()
+    while 1:
+        c = w1.getch()
+        if c == ord('p'):
+            w2.addstr(0, 0, "Current mode: Typing mode", curses.A_REVERSE)
+            w2.refresh()
+        elif c == ord('q'):
+            break  # Exit the while()
 
-while 1:
-    c = stdscr.getch()
-    if c == ord('p'):
-        stdscr.addstr("Pete")
-    elif c == ord('q'):
-        break  # Exit the while()
 
-curses.nocbreak(); stdscr.keypad(0); curses.echo()
-curses.endwin()
+curses.wrapper(main)
