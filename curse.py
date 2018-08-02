01d41171983cf02b04198013ad1ab517e82f7d0f
Pete Richardson <peterichardson@apple.com>
2018-08-02 22:44:46 +0100

    got some windows working a bit

--- curse.py
+++ curse.py
@@ -1,19 +1,65 @@
 #!/usr/bin/env python
 import curses
+import random
+
+numbers1 = []
+numbers2 = []
+
+def prep_data():
+    for i in xrange(10):
+        numbers1.append(str(random.random()*40))
+        numbers2.append(str(random.random()*40))
+
+def add_list_to_window(window, numlist):
+    txt = "\n".join(numlist)
+    #txt = "pete"
+    window.addstr(1,1,txt)
+
+def win1(y,x,stdscr):
+    w1 = stdscr.subwin(18,x/2,0,0)
+    w1.box()
+    w1.bkgd(' ', curses.color_pair(1))
+    add_list_to_window(w1,numbers1)
+    w1.refresh()
+    return w1
+
+def win2(y,x,stdscr):
+    w2 = stdscr.subwin(18,x/2,0,x/2)
+    w2.box()
+    w2.bkgd(' ', curses.color_pair(2))
+    add_list_to_window(w2,numbers1)
+    w2.refresh()
+    return w2
 
 def main(stuff):
-    w1 = curses.newwin(5,20,0,0)
-    w2 = curses.newwin(5,20,0,21)
-    w1.addstr(0,2,"w1")
-    w2.addstr(0,2,"w2",curses.A_REVERSE)
-    w2.refresh()
-    while 1:
-        c = w1.getch()
-        if c == ord('p'):
-            w2.addstr(0, 0, "Current mode: Typing mode", curses.A_REVERSE)
+    NO_KEY_PRESSED = -1
+
+    prep_data()
+
+    stdscr = curses.initscr()
+    stdscr.nodelay(True)
+
+    # Colors
+    if curses.has_colors():
+        curses.start_color()
+        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
+        curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_RED)
+        curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_YELLOW)
+        curses.init_pair(4, curses.COLOR_BLACK, curses.COLOR_BLUE)
+
+    y, x = stdscr.getmaxyx()
+    w1 = win1(y,x,stdscr)
+    w2 = win2(y,x,stdscr)
+
+    key_pressed = NO_KEY_PRESSED
+    while key_pressed != ord('q'):
+        key_pressed = stdscr.getch()
+
+        if key_pressed == ord('p'):
+            w2.addstr(1,1, "Current mode: Typing mode", curses.A_REVERSE)
             w2.refresh()
-        elif c == ord('q'):
-            break  # Exit the while()
+
+        stdscr.refresh()
 
 
 curses.wrapper(main)
