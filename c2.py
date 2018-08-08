f4f564f1584536298d8a5ed87d4673b5ed86d978
Unknown <pete@peterichardson.com>
2018-08-08 11:37:41 +0100

    Abstract simple boxed window
    Should refactor into window and boxed window.

--- c2.py
+++ c2.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python
 import curses
 
-y,x = -1,-1
+class Window:
+    def __init__(self, height, width, top, left, stdscr):
+        self.win = stdscr.subwin(height, width, top, left)
+        self.win.box()
+        self.content =  stdscr.subwin(height-2, width-2, top+1, left+1)
 
 def main(stdscr):
 
     NO_KEY_PRESSED = -1
 
     y, x = stdscr.getmaxyx()
-    w = stdscr.subwin(y-1,x-1,1,1)
-    w.addstr(0,0,"Pete Richardson")
-    w.refresh()
 
+    lwin = Window(y,x/2,0,0,stdscr).content
+    lwin.addstr(0,0,"Pete Richardson")
+    rwin = Window(y,x/2,0,x/2,stdscr).content
+    rwin.addstr(0,0,"Pete Richardson")
+    lwin.addstr(1,0,"Wendy Wilson")
+    
     key_pressed = NO_KEY_PRESSED
     while key_pressed != ord('q'):
         key_pressed = stdscr.getch()
